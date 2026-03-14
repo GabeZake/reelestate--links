53e6ca67fe54dfcbc56b1f7a391d33a14d4e9d11
GabeZake <48073539+GabeZake@users.noreply.github.com>
2026-03-14 16:12:23 -0400

    Update apple​-app​-site​-association​.js

--- api/.well​-known​/apple​-app​-site​-association​.js
+++ api/.well​-known​/apple​-app​-site​-association​.js
@@ -1,1 +1,14 @@
-
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json({
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: "HZ35J2AG79.com.beakericestudios.ReelEstate1",
+          paths: ["/listing/*", "/reel/*"]
+        }
+      ]
+    }
+  });
+}
